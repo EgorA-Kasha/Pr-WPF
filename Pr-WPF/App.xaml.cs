bb17d002171d3f6737f1af7085dd911af11864bf
234824 <234824@KB37-118-C13>
2026-03-26 09:03:48 +0300

    kip

--- Pr-WPF/App.xaml.cs
+++ Pr-WPF/App.xaml.cs
@@ -13,6 +13,6 @@ namespace Pr_WPF
     /// </summary>
     public partial class App : Application
     {
-        public static Entities db = new Entities();
+        public static Entities1 db = new Entities1();
     }
 }
