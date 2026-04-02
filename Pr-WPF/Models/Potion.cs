8311390126d402ca154a73b97b4e34a99c484172
234824 <234824@KB37-118-C13>
2026-04-02 14:07:41 +0300

    Добавлено зелье исцеления.

---
+++ Pr-WPF/Models/Potion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr_WPF.Models
+{
+    internal class Potion : Item
+    {
+        public Potion()
+        {
+            Name = "Зелье исцеления";
+            ImageName = "potion.jpg";
+        }
+    }
+}
